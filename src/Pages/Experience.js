a0a41c89181ad4987fccd1792d9727420f5d5ae6
Ali Cem GEZER <132958533+cemgezerr@users.noreply.github.com>
2023-08-05 23:43:38 +0300

    experinece

---
+++ src/Pages/Experience.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import SchoolIcon from '@mui/icons-material/School';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+
+const Experience = () => {
+  return (
+    <div className='container'>
+      <div className='expElements'>
+        <ol>
+          <li>
+            <SchoolIcon />
+            <h3>High Scholl Graduation 2020</h3>
+            <p>Graduated from the department of mathematics of Ataturk University</p>
+          </li>
+        </ol>
+      </div>
+    </div>
+  )
+}
+
+export default Experience
