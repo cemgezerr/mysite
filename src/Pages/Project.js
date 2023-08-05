296fd7730aeac90248c85480c8bc820eede931a5
Ali Cem GEZER <132958533+cemgezerr@users.noreply.github.com>
2023-08-05 23:43:52 +0300

    projects

---
+++ src/Pages/Project.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ProjectItems from '../components/ProjectItems'
+import { ProjectList } from '../helper/Projects'
+
+const Project = () => {
+  return (
+    <div className='projects'>
+      <h1>My Personal Projects</h1>
+      <div className='projectList'>
+        {ProjectList.map((project) => {
+          return <ProjectItems name={project.name} image={project.image} />
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Project
